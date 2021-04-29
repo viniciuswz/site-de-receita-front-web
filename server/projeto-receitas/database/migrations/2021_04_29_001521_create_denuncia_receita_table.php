f947427b2e5aaebb9be6f914048d228397af3188
Nathan Garcia <nathangar01@gmail.com>
2021-04-28 21:18:40 -0300

    Migration denuncia_receita

---
+++ server/projeto-receitas/database/migrations/2021_04_29_001521_create_denuncia_receita_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDenunciaReceitaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('denuncia_receita', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('usuario_id')->constrained('usuario');
+            $table->foreignId('receita_id')->constrained('receita');
+
+            $table->text('descricao');
+            $table->text('coment_resolucao');
+            $table->boolean('status_denuncia');
+            $table->boolean('status_ativo');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('denuncia_receita');
+    }
+}
