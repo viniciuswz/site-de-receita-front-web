37ccbfc3f276bf067a66278f02de6e0dac51df09
Nathan Garcia <nathangar01@gmail.com>
2021-04-30 19:04:36 -0300

    Migrations sobre o comentario

---
+++ server/projeto-receitas/database/migrations/2021_04_30_215213_create_curtida_comentario_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCurtidaComentarioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('curtida_comentario', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('status_ativo');
+            $table->foreignId('usuario_id')->constrained('usuario');
+            $table->foreignId('comentario_id')->constrained('comentario');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('curtida_comentario');
+    }
+}
