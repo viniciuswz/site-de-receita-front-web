f93404b22f8d047ca523f565d8708786aea508fb
Nathan Garcia <nathangar01@gmail.com>
2021-04-29 23:37:47 -0300

    Migrations sobre avaliacao

---
+++ server/projeto-receitas/database/migrations/2021_04_30_022337_create_curtida_avaliacao_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCurtidaAvaliacaoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('curtida_avaliacao', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('usuario_id')->constrained('usuario');
+            $table->foreignId('avaliacao_id')->constrained('avaliacao');
+            $table->boolean('status_ativo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('curtida_avaliacao');
+    }
+}
