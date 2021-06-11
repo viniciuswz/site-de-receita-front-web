11d5d0d7bf71e1a7301c0f1e5e65e50b8f63c145
Daniel52x <danielcost0705@gmail.com>
2021-06-10 23:10:32 -0300

    Seeder Tipo Usuario

--- server/projeto-receitas/database/seeders/DatabaseSeeder.php
+++ server/projeto-receitas/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call([
+            TipoUser::class
+        ]);
         // \App\Models\User::factory(10)->create();
     }
 }
