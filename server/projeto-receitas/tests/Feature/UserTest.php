80ef6cac2dd2b0699ddd69dc1ebeea043a333729
Daniel52x <danielcost0705@gmail.com>
2021-06-12 15:14:21 -0300

    Test: Insert User

--- server/projeto-receitas/tests/Feature/UserTest.php
+++ server/projeto-receitas/tests/Feature/UserTest.php
@@ -8,6 +8,7 @@ use Tests\TestCase;
 
 use App\Libraries\Users\UserLib;
 use App\Libraries\Users\Protocols\UserProtocol;
+use Illuminate\Support\Str;
 
 class UserTest extends TestCase
 {
@@ -16,22 +17,28 @@ class UserTest extends TestCase
      *
      * @return void
      */
-    // public function test_return_user_protocol_insert_user()
-    // {
-    //     $user = new UserLib();
-    //     $userProtocol = new UserProtocol();
-    //     $this->assertInstanceOf('\App\Models\User', $user->insertWithSendEmail($userProtocol));
-    // }
+    public function test_return_user_protocol_insert_user()
+    {
+        $user = new UserLib();
+        $userProtocol = new UserProtocol();
+        $userProtocol->setName(Str::random(10));
+        $userProtocol->setEmail(Str::random(10).'@gmail.com');
+        $userProtocol->setPassword(Str::random(10));
+        $userProtocol->setImgPerfil(Str::random(10));
+        $userProtocol->setImgCapa(Str::random(10));
+        $userProtocol->setTipoUsuarioId(1);
+        $this->assertInstanceOf('\App\Models\User', $user->insertWithSendEmail($userProtocol));
+    }
 
-    // public function test_param_insert_user_is_type_user_protocol()
-    // {
+    public function test_param_insert_user_is_type_user_protocol()
+    {
 
-    //     $observer = $this->getMockBuilder(UserLib::class)->getMock();
+        $observer = $this->getMockBuilder(UserLib::class)->getMock();
 
-    //     $observer->expects($this->once())
-    //              ->method('insertWithSendEmail')
-    //              ->with(new UserProtocol());
+        $observer->expects($this->once())
+                 ->method('insertWithSendEmail')
+                 ->with(new UserProtocol());
 
-    //     $observer->insertWithSendEmail(new UserProtocol());
-    // }
+        $observer->insertWithSendEmail(new UserProtocol());
+    }
 }
