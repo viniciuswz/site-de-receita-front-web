823307e7932c910c680d1c92bafbaa9d0f181126
Viniciuswz <vinicius-araujo0@hotmail.com>
2021-08-08 17:05:03 -0300

    feature: initial project base with eslint, prettier and editor config

---
+++ web/site-de-receitas/src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '../styles/globals';
+import theme from '../styles/theme';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+export default MyApp;
---
+++ web/site-de-receitas/src/styles/theme.ts
@@ -0,0 +1,8 @@
+const theme = {
+  colors: {
+    background: '#e1e1e6',
+    text: '#000',
+  },
+};
+
+export default theme;
