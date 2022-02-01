78565beb4115b5f16ecde61610d09b9e25d02a88
Daniel Costa <32081901+Daniel52x@users.noreply.github.com>
2022-02-01 00:28:43 -0300

    Testes unitários na pasta de teste unitarios

--- server/projeto-receitas/tests/Unit/InsertUserTest.php
+++ server/projeto-receitas/tests/Unit/InsertUserTest.php
@@ -1,18 +1,72 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests\Feature;
 
-use PHPUnit\Framework\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Support\Str;
+
+use App\Libraries\Users\Features\Mocks\InsertUserMock;
+use App\Libraries\Users\Protocols\UserProtocol;
+use App\Libraries\Users\Exceptions\ExceptionLib\InsertUserException;
+use Illuminate\Http\UploadedFile;
 
 class InsertUserTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    protected function createImageFake($type){
+        $file = UploadedFile::fake();
+        return $file->create('fake_image', 0, $type);
+    }
+
+    public function test_param_insert_user_is_type_user_protocol()
     {
-        $this->assertTrue(true);
+        $observer = $this->getMockBuilder(InsertUserMock::class)->getMock();
+
+        $observer->expects($this->once())
+                 ->method('insertWithSendEmail')
+                 ->with(new UserProtocol());
+
+        $observer->insertWithSendEmail(new UserProtocol());
+    }
+
+    public function test_insert_user_returns_exception_because_user_protocol_empty(){
+        $this->expectException(InsertUserException::class);
+        $this->expectExceptionCode(400);
+        $insertUser = new InsertUserMock();
+        $userProtocol = new UserProtocol();
+        $insertUser->insertWithSendEmail($userProtocol);
+    }
+
+    public function test_retuns_exception_because_email_duplicate(){
+        $this->expectException(InsertUserException::class);
+        $this->expectExceptionCode(400);
+        $this->expectExceptionMessage("O E-mail informado já existe em nossa base de dados");
+
+        $userProtocol = new UserProtocol();
+        $userProtocol->setName(Str::random(10) . ' ' . Str::random(10));
+        $userProtocol->setEmail('invalid@valid.com.br');
+        $userProtocol->setPassword(Str::random(10));
+        $userProtocol->setImgPerfil($this->createImageFake('image/png'), 'user-perfil-test');
+        $userProtocol->setImgCapa($this->createImageFake('image/png'), 'user-capa-test');
+        $userProtocol->setTipoUsuarioId(UserProtocol::$TIPO_USUARIO_COMUM);
+
+        $insertUser = new InsertUserMock();
+        $insertUser->insertWithSendEmail($userProtocol);
+        $insertUser->insertWithSendEmail($userProtocol);
+    }
+
+    public function test_return_id_user_insert_user()
+    {
+        $insertUser = new InsertUserMock();
+        $userProtocol = new UserProtocol();
+        $userProtocol->setName(Str::random(10) . ' ' . Str::random(10));
+        $userProtocol->setEmail('email@valid.com.br');
+        $userProtocol->setPassword(Str::random(10));
+        $userProtocol->setImgPerfil($this->createImageFake('image/png'), 'user-perfil-test');
+        $userProtocol->setImgCapa($this->createImageFake('image/png'), 'user-capa-test');
+        $userProtocol->setTipoUsuarioId(UserProtocol::$TIPO_USUARIO_COMUM);
+
+        $this->assertIsInt($insertUser->insertWithSendEmail($userProtocol));
     }
 }
